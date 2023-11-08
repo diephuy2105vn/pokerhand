de7398ddda5bb5138de3d30e0af744e5f0e173c8
diephuy2105vn <tuanhuy2005vn@gmail.com>
2023-11-08 17:14:02 +0700

    Code nè

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import classification_report
 from sklearn.model_selection import KFold
 from sklearn.metrics import f1_score
+from imblearn.over_sampling import SMOTE
 
 app = Flask(__name__)
 
@@ -19,7 +19,6 @@ def sort_pairs_in_row(row):
     sorted_pairs = sorted(pairs, key=lambda x: (x[1], x[0]))
     return sorted_pairs
 
-
 def sort_data(data):
     data = pd.DataFrame(data)
     sorted_data = data.apply(sort_pairs_in_row, axis=1)
@@ -29,47 +28,41 @@ def sort_data(data):
 def train_model(): 
     data = pd.read_csv("./data/poker-hand-training-true.data", delimiter=",", header=None)
     print(len(data))
+    
     kf = KFold(n_splits=10)
     X= sort_data(data.iloc[:,0:10])
     Y = data.iloc[:, -1]
+    
     modalResults = None
-    rateMax = 0
     f1Avg = 0
-    rateAvg = 0
+    f1Max = 0
     for train_index, test_index in kf.split(data):
         print("Train:", train_index, "Test:", test_index)
         X_train, X_test = X[train_index], X[test_index]
         Y_train, Y_test = Y[train_index], Y[test_index]
+        
         knn = KNeighborsClassifier(n_neighbors=5)
         knn.fit(X_train, Y_train)
         bayes = MultinomialNB()
         bayes.fit(X_train, Y_train)
-        clf = DecisionTreeClassifier(criterion='gini', random_state=42)
+        clf = DecisionTreeClassifier(criterion='gini', random_state=42, class_weight={0: 49.89, 1: 57.75, 2: 95.25, 3: 97.89, 4: 99.61, 5: 99.81, 6: 99.86, 7: 99.97, 8: 99.998, 9: 99.99})
         clf.fit(X_train, Y_train)
         Y_pred_KNN = knn.predict(X_test)
         Y_pred_Bayes = bayes.predict(X_test)
         Y_pred_Clf = clf.predict(X_test)
-        # Tính F1 score cho mô hình Bayes
         f1_score_Bayes = round(f1_score(Y_test, Y_pred_Bayes, average='macro'),4)
         f1_score_KNN = round(f1_score(Y_test, Y_pred_KNN, average='macro'),4)
         f1_score_Clf = round(f1_score(Y_test, Y_pred_Clf, average='macro'),4)
         f1Avg += f1_score_Clf
-        rate_KNN = round(accuracy_score(Y_test, Y_pred_KNN), 4)
-        rate_Bayes = round(accuracy_score(Y_test, Y_pred_Bayes), 4)
-        rate_Clf = round(accuracy_score(Y_test, Y_pred_Clf), 4)
-        rateAvg += rate_Clf
-        print("Độ chính xác KNN:", rate_KNN)
-        print("Độ chính xác Bayes", rate_Bayes)
-        print("Độ chính xác Tree", rate_Clf)
         print("Chỉ số F1 KNN", f1_score_KNN)
-        if(rateMax < rate_Clf):
-            rateMax = rate_Clf
-            modalResults = clf
         print("Chỉ số F1 Bayes",  f1_score_Bayes)
         print("Chỉ số F1 Tree",  f1_score_Clf)
-    
+        if(f1Max < f1_score_Clf):
+            modalResults = clf
+        # print(classification_report(Y_test, Y_pred_Clf)), 
+    print("Chỉ số F1 trung bình", round(f1Avg/10 *100, 4) )
     print("Độ chính xác tổng thể", round(rateAvg/10 *100, 4) )
-    return modalResults, rateMax
+    return modalResults
 
 @app.route("/")
 def main():
@@ -78,12 +71,7 @@ def main():
 @app.route('/forecast/', methods=['POST'])
 def get_post_data(): 
     data = request.get_json()
-    data_test = []
-    for i in range(1, 6):
-        rank_key = f'rank{i}'
-        card_key = f'card{i}'
-        data_test.extend([data[rank_key], data[card_key]])
-    data_test = sort_data([data_test])
+    data_test = sort_data([data])
     pred = clf.predict(data_test).tolist()
     message = ""
     match pred[0]: 
